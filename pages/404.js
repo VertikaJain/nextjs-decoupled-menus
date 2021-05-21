dd52002236030dec4bc45151a0e722dd89ea9b02
Vertika Jain <vertikajain96@gmail.com>
2021-05-21 16:08:16 +0530

    Redirect to home page using useRouter()

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,20 @@
 import Link from "next/link"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 const File404 = () => {
+
+    const router = useRouter()
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/")
+        }, 5000);
+    }, [])
+
     return <div className="not-found">
         <h1>The Page cannot be Found.</h1>
         <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
     </div>
 }
- 
+
 export default File404;
